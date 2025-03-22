23a03aff986b77259c624b6917b214870c2d25dd
MaciejKlepacki <maciek.klepacki@icloud.com>
2025-03-22 22:36:34 +0100

    Adding 02_stacks

--- 02_stacks/stck_template.c
+++ 02_stacks/stck_template.c
@@ -8,12 +8,22 @@ int stack[STACK_SIZE];
 int top = 0;
 
 int stack_push(int x) {
+	if (top == STACK_SIZE) return OVERFLOW;
+	stack[top] = x;
+	top++;
+	return OK;
 }
 
 int stack_pop(void) {
+	if (top == 0) return UNDERFLOW;
+	top--;
+	int result = stack[top];
+	stack[top] = 0;
+	return result;
 }
 
 int stack_state(void) {
+	return top;
 }
 
 // FIFO queue with shifts
@@ -23,16 +33,46 @@ int stack_state(void) {
 int queue[QUEUE_SIZE];
 int in = 0, curr_nr = 0;
 
-int queue_push(int in_nr) { // in_nr clients try to enter the queue
+int queue_push(int in_nr) { // in_nr klientów próbuje wejść do kolejki
+    int over = 0;
+	for (int i = 0; i < in_nr; i++){
+		curr_nr++;
+		if (in == QUEUE_SIZE) over = 1;
+		else{
+			queue[in] = curr_nr;
+			in++;
+		}
+	}
+	if (over) return OVERFLOW;
+	else return 0;
 }
 
-int queue_pop(int out_nr) { // out_nr clients leaves the queue
+int queue_pop(int out_nr) { // out_nr klientów opuszcza kolejkę
+    if (out_nr > in) {
+        in = 0;
+        return UNDERFLOW;
+    }
+    // Przepisanie pozostałych elementów do początku tablicy
+    for (int i = 0; i < in - out_nr; i++) {
+        queue[i] = queue[i + out_nr];
+    }
+    // Opcjonalne wyczyszczenie "starych" pozycji
+    for (int i = in - out_nr; i < in; i++) {
+        queue[i] = 0;
+    }
+    in -= out_nr;
+    return in;
 }
 
 int queue_state(void) {
+	return in;
 }
 
 void queue_print(void) {
+	for (int i = 0; i < in; i++){
+		printf("%d ", queue[i]);
+	}
+	printf("\n");
 }
 
 // Queue with cyclic buffer
@@ -44,15 +84,29 @@ int out = 0, len = 0;
 
 
 int cbuff_push(int cli_nr) { // client with number cli_nr enters the queue
+	if (len == CBUFF_SIZE) return OVERFLOW;
+	cbuff[(out + len) % CBUFF_SIZE] = cli_nr;
+	len++;
+	return OK;
 }
 
 int cbuff_pop(void) { // longest waiting client leaves the queue
+	if (len==0) return UNDERFLOW;
+	int result = cbuff[out];
+	out = (out + 1) % CBUFF_SIZE;
+    len--;
+    return result;
 }
 
 int cbuff_state(void) {
+	return len;
 }
 
 void cbuff_print(void) {
+	for (int i = out; i < len + out; i++){
+		printf("%d ", cbuff[i % CBUFF_SIZE]);
+	}
+	printf("\n");
 }
 
 int main(void) {
